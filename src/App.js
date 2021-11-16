b53e2c51e35f16d96b0f9ce336caada26b0f9db1
Jaylen Wilson <Jaylenaverywilson@gmail.com>
2021-11-16 18:49:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import SiteBar from './home/Navbar';
+import Auth from './auth/Auth';
+function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+  return (
+    <div>
+      <SiteBar clickLogout={clearToken} />
+      <Auth updateToken={updateToken} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/auth/Signup.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+const Signup = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventdefault();
+        console.log('hello world')
+
+        fetch('http://localhost:3000/user/register', {
+            method: 'POST',
+            body: JSON.stringify({ user: { userName: username, password: password } }),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }).then(
+            (response) => response.json()
+        ).then((data) => {
+            props.updateToken(data.sessionToken)
+        })
+    }
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <Label htmlFor='username'>Username</Label>
+                    <Input onChange={(e) => setUsername(e.target.value)} name='username' value={username} />
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor='password'>Password</Label>
+                    <Input onChange={(e) => setPassword(e.target.value)} name='password' value={password} />
+                </FormGroup>
+            </Form>
+            <Button type='submit'>Sign Up</Button>
+        </div>
+    )
+
+
+}
+
+export default Signup
